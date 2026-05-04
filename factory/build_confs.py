b115f04f8e46a024abc36a95cee1a23fb5309610
Terakomari Gandesblood <57123257+Tooisorahe@users.noreply.github.com>
2026-05-04 17:31:28 +0800

    Update build_confs.py

--- factory/build_confs.py
+++ factory/build_confs.py
@@ -47,7 +47,7 @@ def getRulesStringFromFile(path, kind):
                 prefix = 'DOMAIN-KEYWORD'
 
             if opt:
-                ret += prefix + ',' + f'{content},{kind},{",".join(opt)}'
+                ret += prefix + ',' + f'{content},{kind},{",".join(opt)}\n'
             else:
                 ret += prefix + ',%s,%s\n' % (content, kind)
 
